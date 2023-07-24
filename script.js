dd0312df64a39838bfcf051612f92363128186f5
MarcelNota <n3gr1t0.mn@gmail.com>
2023-07-24 02:15:43 +0200

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  alert("Tem otimos gostos humano")
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/marcel-light.png")
+  } else {
+    img.setAttribute("src", "./assets/m.png")
+  }
+}
